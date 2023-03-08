e03f45dd9d5c77300981d08a9b47eb05ec47029a
sergiomll1968 <sergiomaciaslluch@gmail.com>
2023-03-08 19:35:12 +0100

    Sigo actualizando practicas

--- 12-js/cajero2.js
+++ 12-js/cajero2.js
@@ -1,11 +1,19 @@
-const cashRequested = 162.39;
-const cash = ['500','200','100','50','20','10','5','2','1','05','02','01','005','002','001'];
+// const cashRequested = 162.39;
+const cash = [500, 200, 100, 50, 20, 10, 5, 2, 1];
 
 const start = 'bill';
-const end = 'Quantity';
 
 for (let i = 0; i < cash.length; i++) {
-  console.log(cash[i]);
-  eval('let ' + start + cash[i] + end + '= ' + cashRequested + ';');
-  console.log(eval('let ' + start + cash[i] + end + '= ' + cashRequested + ';'));
+  document.write(cash[i] + '<br>');
+  // eval('let ' + start + cash[i] + ' = ' + 'zzz' + ';');
 }
+
+var k = 'value';
+var i = 0;
+for (i = 1; i < 5; i++) {
+  eval('var ' + k + i + '= ' + i + ';');
+}
+console.log("value1=" + value1);
+console.log("value2=" + value2);
+console.log("value3=" + value3);
+console.log("value4=" + value4);
